c8b4453afe5ef880cb42da6ea46708d543e43dd0
arkvidd <dylan0steven@gmail.com>
2025-10-31 11:10:13 -0500

    actualizacion de react version 3

---
+++ ex_03/src/components/agregarTarea.jsx
@@ -0,0 +1,9 @@
+import {useState} from 'react'
+
+import react from 'react'
+
+export const agregarTarea = () => {
+    return (
+        <div>agregarTarea</div>
+    )
+}
---
+++ ex_03/src/listadoapp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {useState} from 'react'
+
+export const listadoapp = () => {
+  return (
+    <div>listadoapp</div>
+  )
+}
